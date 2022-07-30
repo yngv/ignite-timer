00f95dba4a0803cf2b9206e677fdd907f8f8d2be
yngv <ravgny@protonmail.com>
2022-07-30 01:34:25 -0300

    add react-router routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
     </ThemeProvider>
   )
 }
